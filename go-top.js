427c1b591e8012f7beef4861f2817dec695fd0f9
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-05-17 02:08:44 +0300

    arrows animation added

--- go-top.js
+++ go-top.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('body').append(
-    '<a href="#" id="go-top" title="Вверх"><div class="go-top-arrow"></div></a>',
+    '<a href="#" id="go-top" tabindex="0" title="Вверх"><div class="go-top-arrow"></div></a>',
   );
 });
 
